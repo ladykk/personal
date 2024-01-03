4a8cf0a06352062d67fd4fec727cca6b2aa1932c
Rattapon Apiratjit <krtp.apirat@gmail.com>
2024-01-03 12:43:11 +0700

    @core/middleware: fix rewrite issue

--- src/middleware.ts
+++ src/middleware.ts
@@ -12,9 +12,8 @@ export const config = {
      * 2. /_next (Next.js internals)
      * 3. /_static (inside /public)
      * 4. all root files inside /public (e.g. /favicon.ico)
-     * 5. /file (storage subdomain)
      */
-    "/((?!api/|_next/|_static/|_vercel|file/|[\\w-]+\\.\\w+).*)",
+    "/((?!api/|_next/|_static/|_vercel|[\\w-]+\\.\\w+).*)",
   ],
 };
 
